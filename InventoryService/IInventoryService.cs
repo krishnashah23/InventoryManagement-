250f35489a9c22e7d7988e336cbf35ba20927c58
krishnashah23 <68641771+krishnashah23@users.noreply.github.com>
2021-02-19 22:51:54 +0530

    Service Files

---
+++ InventoryService/IInventoryService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryService
+{
+    [ServiceContract]
+    public interface IInventoryService
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        string AddProduct(Product prod);
+
+        [OperationContract]
+        DataSet GetProducts();
+
+        [OperationContract]
+        string DeleteProduct(Product prod);
+
+        [OperationContract]
+        DataSet SearchProduct(Product prod);
+
+        [OperationContract]
+        string UpdateProduct(Product prod);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Product
+    {
+        string _prodID = "";
+        string _name = "";
+        string _category = "";
+        int _quantity = 0;
+        int _price = 0;
+        string _supplier = "";
+
+        [DataMember]
+        public string ProdID
+        {
+            get { return _prodID; }
+            set { _prodID = value; }
+        }
+
+        [DataMember]
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        [DataMember]
+        public string Category
+        {
+            get { return _category; }
+            set { _category = value; }
+        }
+
+        [DataMember]
+        public int Quantity
+        {
+            get { return _quantity; }
+            set { _quantity = value; }
+        }
+
+        [DataMember]
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        [DataMember]
+        public string Supplier
+        {
+            get { return _supplier; }
+            set { _supplier = value; }
+        }
+    }
+}
---
+++ InventoryService/InventoryService.cs
@@ -0,0 +1,140 @@
+﻿using InventoryManagementSystem.MyService;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryService
+{
+    public class InventoryService : IInventoryService
+    {
+        public string AddProduct(Product prod)
+        {
+            string result = "";
+            try
+            {
+
+                SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                SqlCommand cmd = new SqlCommand();
+
+                string Query = @"INSERT INTO tblEmployee (EmpID,Name,Email,Phone,Gender)  
+                                               Values(@EmpID,@Name,@Email,@Phone,@Gender)";
+
+                cmd = new SqlCommand(Query, con);
+                cmd.Parameters.AddWithValue("@ProdID", prod.ProdID);
+                cmd.Parameters.AddWithValue("@Name", prod.Name);
+                cmd.Parameters.AddWithValue("@Category", prod.Category);
+                cmd.Parameters.AddWithValue("@Quantity", prod.Quantity);
+                cmd.Parameters.AddWithValue("@Price", prod.Price);
+                cmd.Parameters.AddWithValue("@Supplier", prod.Supplier);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                result = "Record Added Successfully !";
+            }
+            catch (FaultException fex)
+            {
+                result = "Error";
+            }
+
+            return result;
+        }
+        public DataSet GetProducts()
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                string Query = "SELECT * FROM products";
+
+                SqlDataAdapter sda = new SqlDataAdapter(Query, con);
+                sda.Fill(ds);
+            }
+            catch (FaultException fex)
+            {
+                throw new FaultException<string>("Error: " + fex);
+            }
+
+            return ds;
+        }
+        public string DeleteProduct(Product prod)
+        {
+            string result = "";
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlCommand cmd = new SqlCommand();
+            SqlCommand cmd1 = new SqlCommand();
+            string Query1 = "Select Quantity from products where ProdID=@ProdID";
+            string Query = "DELETE FROM products Where ProdID=@ProdID";
+            string Query2 = "update products set quantity=quantity=quantity-1 where ProdID=@ProdID";
+            cmd = new SqlCommand(Query, con);
+            cmd1 = new SqlCommand(Query1, con);
+            SqlCommand cmd2 = new SqlCommand(Query2, con);
+            cmd1.Parameters.AddWithValue("@ProdID", prod.ProdID);
+            cmd.Parameters.AddWithValue("@ProdID", prod.ProdID);
+            cmd.Parameters.AddWithValue("@ProdID", prod.ProdID);
+            con.Open();
+            SqlDataReader rd = cmd1.ExecuteReader();
+            while (rd.Read())
+            {
+                if ((int)rd[0] > 1)
+                {
+                    cmd2.ExecuteNonQuery();
+                }
+                else
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+            con.Close();
+            result = "Record Deleted Successfully!";
+            return result;
+        }
+        public DataSet SearchProduct(Product prod)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                string Query = "SELECT * FROM products WHERE ProdID=@ProdID";
+
+                SqlDataAdapter sda = new SqlDataAdapter(Query, con);
+                sda.SelectCommand.Parameters.AddWithValue("@ProdID", prod.ProdID);
+                sda.Fill(ds);
+            }
+            catch (FaultException fex)
+            {
+                throw new FaultException<string>("Error:  " + fex);
+            }
+            return ds;
+        }
+        public string UpdateProduct(Product prod)
+        {
+            string result = "";
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlCommand cmd = new SqlCommand();
+
+            string Query = "UPDATE products SET Quantity=@Quantity,Price=@Price WHERE ProdID=@ProdID";
+
+            cmd = new SqlCommand(Query, con);
+            cmd.Parameters.AddWithValue("@ProdID", prod.ProdID);
+            cmd.Parameters.AddWithValue("@Quantity", prod.Quantity);
+            cmd.Parameters.AddWithValue("@Price", prod.Price);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            result = "Record Updated Successfully !";
+            con.Close();
+
+            return result;
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
